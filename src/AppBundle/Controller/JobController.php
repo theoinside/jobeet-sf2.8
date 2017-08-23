dbf6d77b05d93e2e83f80dd7cdba98178aa44357
theoinside <unescapist@gmail.com>
2017-08-23 22:45:58 +0300

    routing rebuilt

--- src/AppBundle/Controller/JobController.php
+++ src/AppBundle/Controller/JobController.php
@@ -3,9 +3,11 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Job;
+use AppBundle\Form\JobType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Job controller.
@@ -16,9 +18,6 @@ class JobController extends Controller
 {
     /**
      * Lists all job entities.
-     *
-     * @Route("/", name="job_index")
-     * @Method("GET")
      */
     public function indexAction()
     {
@@ -26,32 +25,29 @@ class JobController extends Controller
 
         $jobs = $em->getRepository('AppBundle:Job')->findAll();
 
-        return $this->render('job/index.html.twig', array(
+        return $this->render('AppBundle:Job:index.html.twig', array(
             'jobs' => $jobs,
         ));
     }
 
     /**
      * Creates a new job entity.
-     *
-     * @Route("/new", name="job_new")
-     * @Method({"GET", "POST"})
      */
     public function newAction(Request $request)
     {
         $job = new Job();
-        $form = $this->createForm('AppBundle\Form\JobType', $job);
+        $form = $this->createForm(JobType::class, $job);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $em->persist($job);
             $em->flush();
 
-            return $this->redirectToRoute('job_show', array('id' => $job->getId()));
+            return $this->redirectToRoute('app_bundle_job_show', array('id' => $job->getId()));
         }
 
-        return $this->render('job/new.html.twig', array(
+        return $this->render('AppBundle:Job:new.html.twig', array(
             'job' => $job,
             'form' => $form->createView(),
         ));
@@ -59,15 +55,12 @@ class JobController extends Controller
 
     /**
      * Finds and displays a job entity.
-     *
-     * @Route("/{id}", name="job_show")
-     * @Method("GET")
      */
     public function showAction(Job $job)
     {
         $deleteForm = $this->createDeleteForm($job);
 
-        return $this->render('job/show.html.twig', array(
+        return $this->render('AppBundle:Job:show.html.twig', array(
             'job' => $job,
             'delete_form' => $deleteForm->createView(),
         ));
@@ -75,23 +68,20 @@ class JobController extends Controller
 
     /**
      * Displays a form to edit an existing job entity.
-     *
-     * @Route("/{id}/edit", name="job_edit")
-     * @Method({"GET", "POST"})
      */
     public function editAction(Request $request, Job $job)
     {
         $deleteForm = $this->createDeleteForm($job);
-        $editForm = $this->createForm('AppBundle\Form\JobType', $job);
+        $editForm = $this->createForm(JobType::class, $job);
         $editForm->handleRequest($request);
 
         if ($editForm->isSubmitted() && $editForm->isValid()) {
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('job_edit', array('id' => $job->getId()));
+            return $this->redirectToRoute('app_bundle_job_edit', array('id' => $job->getId()));
         }
 
-        return $this->render('job/edit.html.twig', array(
+        return $this->render('AppBundle:Job:edit.html.twig', array(
             'job' => $job,
             'edit_form' => $editForm->createView(),
             'delete_form' => $deleteForm->createView(),
@@ -100,22 +90,19 @@ class JobController extends Controller
 
     /**
      * Deletes a job entity.
-     *
-     * @Route("/{id}", name="job_delete")
-     * @Method("DELETE")
      */
     public function deleteAction(Request $request, Job $job)
     {
         $form = $this->createDeleteForm($job);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $em->remove($job);
             $em->flush();
         }
 
-        return $this->redirectToRoute('job_index');
+        return $this->redirectToRoute('app_bundle_job_index');
     }
 
     /**
@@ -128,7 +115,7 @@ class JobController extends Controller
     private function createDeleteForm(Job $job)
     {
         return $this->createFormBuilder()
-            ->setAction($this->generateUrl('job_delete', array('id' => $job->getId())))
+            ->setAction($this->generateUrl('app_bundle_job_delete', array('id' => $job->getId())))
             ->setMethod('DELETE')
             ->getForm()
         ;
